d501cc22d95d26c3fc77388c770877b4b57a0626
Himatya <Himatya@iMac.local>
2017-12-13 04:49:08 +0900

    add ref_ptr

---
+++ mk2/pointer.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Himatya on 2017/12/13.
+//
+
+#ifndef LIBMK2_POINTER_HPP
+#define LIBMK2_POINTER_HPP
+
+#include <mk2/pointer/ref_ptr.hpp>
+
+#endif //LIBMK2_POINTER_HPP
---
+++ mk2/pointer/ref_ptr.hpp
@@ -0,0 +1,64 @@
+//
+// Created by Himatya on 2017/12/13.
+//
+
+#ifndef LIBMK2_REF_PTR_HPP
+#define LIBMK2_REF_PTR_HPP
+
+#include <memory>
+
+namespace mk2 {
+namespace pointer{
+    template<typename Type>
+    class ref_ptr
+    {
+    public:
+        typedef Type* pointer;
+        typedef Type& reference;
+
+        ref_ptr() : elem_(nullptr)
+        {}
+
+        ref_ptr(pointer ptr) : elem_(ptr)
+        {}
+
+        ref_ptr(std::unique_ptr<Type> &ptr) : elem_(ptr.get())
+        {}
+
+        pointer get() const
+        {
+            return elem_;
+        }
+
+        pointer operator->() const
+        {
+            return elem_;
+        }
+
+        reference operator*() const
+        {
+            return *elem_;
+        }
+
+        operator bool()
+        {
+            return elem_ != nullptr;
+        }
+
+        void reset(pointer p = pointer())
+        {
+            elem_ = p;
+        }
+
+        pointer release()
+        {
+            elem_ = nullptr;
+        }
+
+    private:
+        Type *elem_;
+    };
+}
+}
+
+#endif //LIBMK2_REF_PTR_HPP
