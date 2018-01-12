1c1a1e938811c1b5a9ad43e3f8bf7d84dbb012f7
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-01-12 07:34:46 +0000

    add scoped_thread.hpp

---
+++ mk2/thread/scoped_thread.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Himatya on 2018/01/12.
+//
+
+#ifndef LIBMK2_THREAD_SCOPED_THREAD_HPP
+#define LIBMK2_THREAD_SCOPED_THREAD_HPP
+
+namespace mk2 {
+namespace thread{
+
+}
+}
+
+#endif
