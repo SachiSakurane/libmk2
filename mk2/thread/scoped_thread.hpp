38c0b8484d4beba8baf34be8836eaa6035448ec4
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-01-19 02:45:20 +0000

    edit scoped_thread.hpp

--- mk2/thread/scoped_thread.hpp
+++ mk2/thread/scoped_thread.hpp
@@ -5,7 +5,6 @@
 #ifndef LIBMK2_THREAD_SCOPED_THREAD_HPP
 #define LIBMK2_THREAD_SCOPED_THREAD_HPP
 
-#include <functional>
 #include <thread>
 #include <tuple>
 #include <utility>
@@ -14,29 +13,7 @@
 
 namespace mk2 {
 namespace thread{
-    namespace detail
-    {
-        template<class F>
-        void no_return(F&& f)
-        {
-            std::forward<F>(f)();
-        }
-    
-        template<class F, class Tuple, size_t... Indices>
-        auto bind_func_impl(F&& f, Tuple&& args, std::index_sequence<Indices...>)
-        {
-            return std::bind(no_return, 
-                             std::bind(std::forward<F>(f), std::get<Indices>(std::forward<Tuple>(args))...));
-        }
-    
-        template<class F, class Tuple>
-        auto bind_func(F&& f, Tuple&& tuple)
-        {
-            return bind_func_impl(std::forward<F>(f), std::forward<Tuple>(args),
-                                  std::make_index_sequence<std::tuple_size<Tuple>::value>());
-        }
-    }
-
+    template<class Thread = std::thread>
     class scoped_thread : mk2::utility::noncopyable<scoped_thread>
     {
     public:
@@ -46,30 +23,16 @@ namespace thread{
         template<class F, class... Args>
         scoped_thread(F&& f, Args&&... args) : thread_(std::forward<F>(f), std::forward<Args>(args)...)
         {}
-        
-        template<
-                class ThreadFunc, class ThreadTupleArgs,
-                class FinalizeFunc, class FinalizeTupleArgs
-        >
-        scoped_thread(ThreadFunc&& thread_func, ThreadTupleArgs&& thread_tuple,
-                    FinalizeFunc&& finalize_func, FinalizeTupleArgs&& finalize_tuple)
-            : thread_(mk2::thread::detail::bind_func(thread_func, thread_tuple)),
-              finalizer_(mk2::thread::detail::bind_func(finalize_func, finalize_tuple))
-        {}
 
         scoped_thread(scoped_thread&& obj) noexcept 
-            : thread_{ std::move(obj.thread_) },
-              finalizer_{ std::move(obj.finalizer_) }
+            : thread_{ std::move(obj.thread_) }
         {}
 
         scoped_thread& operator=(scoped_thread&& rhv) noexcept
         {
-            if (finalizer_)
-                finalizer_();
             if (thread_.joinable())
                 thread_.join();
             this->thread_ = std::move(rhv.thread_);
-            this->finalizer_ = std::move(rhv.finalizer_);
             return *this;
         }
         
@@ -80,8 +43,6 @@ namespace thread{
         
         void join()
         {
-            if (finalizer_) 
-                finalizer_();
             if (thread_.joinable())
                 thread_.join();
         }
@@ -92,18 +53,10 @@ namespace thread{
                 thread_.detach();
         }
 
-        std::thread& get_thread() { return thread_; }
-        
-        template<class F, class ...Args>
-        void set_finalizer(F&& f, Args&&... args)
-        {
-            finalizer_ = std::bind(mk2::thread::detail::no_return,
-                                   std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-        }
+        Thread& get_thread() { return thread_; }
 
     private:
-        std::thread thread_;
-        std::function<void()> finalizer_;
+        Thread thread_;
     };
 }
 }
