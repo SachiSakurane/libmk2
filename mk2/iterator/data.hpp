0da6bc28f8c61991f369bab7fc33121056feb1b5
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-01-31 03:19:21 +0000

    add data.hpp

---
+++ mk2/iterator/data.hpp
@@ -0,0 +1,36 @@
+//
+// Created by Himatya on 2018/01/31.
+//
+
+#ifndef LIBMK2_ITERATOR_DATA_HPP
+#define LIBMK2_ITERATOR_DATA_HPP
+
+namespace mk2{
+namespace iterator{
+    template<class Container>
+    constexpr auto data(Container& c) -> decltype(c.size())
+    {
+        return c.data();
+    }
+
+    template<class Container>
+    constexpr auto data(const Container& c) -> decltype(c.size())
+    {
+        return c.data();
+    }
+    
+    template <class T, std::size_t N>
+    constexpr std::size_t data(const T (&array)[N]) noexcept
+    {
+        return array;
+    }
+    
+    template <class E>
+    constexpr const E* data(initializer_list<E> il) noexcept
+    {
+        return il.begin();
+    }
+}
+}
+
+#endif
