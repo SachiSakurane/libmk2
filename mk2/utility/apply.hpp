bafbf24a08e3ae80326bcee4ac3d26f37ba80684
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-02-22 02:45:34 +0000

    edit apply.hpp

--- mk2/utility/apply.hpp
+++ mk2/utility/apply.hpp
@@ -14,27 +14,27 @@ namespace utility {
     namespace detail
     {
         template<class F, class Tuple, size_t... Indices>
-        auto apply_impl(F&& f, const Tuple& args, std::index_sequence<Indices...>)
+        constexpr decltype(auto) apply_impl(F&& f, const Tuple& args, std::index_sequence<Indices...>)
         {
             return std::forward<F>(f)(std::get<Indices>(args)...);
         }
 
         template<class F, class Tuple, size_t... Indices>
-        auto apply_impl(F&& f, Tuple&& args, std::index_sequence<Indices...>)
+        constexpr decltype(auto) apply_impl(F&& f, Tuple&& args, std::index_sequence<Indices...>)
         {
             return std::forward<F>(f)(std::get<Indices>(std::forward<Tuple>(args))...);
         }
     }
 
     template<class F, class Tuple>
-    auto apply(F&& f, const Tuple& args)
+    constexpr decltype(auto) apply(F&& f, const Tuple& args)
     {
         return mk2::utility::detail::apply_impl(std::forward<F>(f), args,
                                                 std::make_index_sequence<std::tuple_size<Tuple>::value>());
     }
 
     template<class F, class Tuple>
-    auto apply(F&& f, Tuple&& args)
+    constexpr decltype(auto) apply(F&& f, Tuple&& args)
     {
         return mk2::utility::detail::apply_impl(std::forward<F>(f), std::forward<Tuple>(args),
                                                 std::make_index_sequence<std::tuple_size<Tuple>::value>());
@@ -44,16 +44,16 @@ namespace utility {
     class apply_functor
     {
     public:
-        explicit apply_functor(F&& f) : f_(std::forward<F>(f)){}
+        constexpr explicit apply_functor(F&& f) : f_(std::forward<F>(f)){}
 
         template<class Tuple>
-        auto operator ()(Tuple&& t)
+        constexpr decltype(auto) operator ()(Tuple&& t)
         {
             return mk2::utility::apply(f_, std::forward<Tuple>(t));
         }
 
         template<class Tuple>
-        auto operator ()(const Tuple& t)
+        constexpr decltype(auto) operator ()(const Tuple& t)
         {
             return mk2::utility::apply(f_, t);
         }
@@ -63,7 +63,7 @@ namespace utility {
     };
 
     template<class F, class Tuple>
-    auto make_apply(F&& f)
+    constexpr decltype(auto) make_apply(F&& f)
     {
         return mk2::utility::apply_functor<F>(std::forward<F>(f));
     }
