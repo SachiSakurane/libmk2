8025a1071edee1234c6b03495678cc4c355b9a21
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-08-16 08:20:59 +0000

    ippm~をしらべる

--- mk2/simd/ipp/lpc.hpp
+++ mk2/simd/ipp/lpc.hpp
@@ -13,14 +13,19 @@ namespace mk2 { namespace simd { namespace ipp
     template<class Type>
     class lpc
     {
+        using complex_type =  mk2::simd::ipp::ipps_complex<Type>;
     public:
         lpc(int order, int size) : 
             order_(order), delay_(order + 1), size_(size),
             r_(delay_),
             a_(delay_), e_(delay_),
-            a_(delay_), e_(delay_)
+            U_(delay_ + 1), V_(delay_ + 1),
+            z_(size * order_)
         {
             assert(order > 0)
+            // z (0, -2 * pi * 0...n / size)
+            // filp( exp(z) )
+            
         }
 
         void process(const Type* src, Type* dst)
@@ -60,7 +65,13 @@ namespace mk2 { namespace simd { namespace ipp
             }
             
             // filter
+            //for ()
+            //complex_type numerator(static_cast<Type>(0), static_cast<Type>(0));
+            //complex_type denominator(static_cast<Type>(0), static_cast<Type>(0));
             
+            //function::ipps_dot_prod(e_.data() - n, z_.data(), numerator, static_cast<int>(e_.size()));
+            //function::ipps_dot_prod(a_.data(), z_.data(), numerator, static_cast<int>(a_.size()));
+            //std::abs(numerator)
         }
 
     private:
@@ -70,6 +81,8 @@ namespace mk2 { namespace simd { namespace ipp
         mk2::container::fixed_array<Type> r_;
         mk2::container::fixed_array<Type> a_, e_;
         mk2::container::fixed_array<Type> U_, V_;
+        mk2::container::fixed_array<complex_type> z_;
+        
     };
 }
 }
