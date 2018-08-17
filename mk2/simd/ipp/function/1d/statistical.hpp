69a208aef460c7aad33c78c7eaf8aabf98a4f248
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-08-17 05:30:35 +0000

    edit statistical.hpp

--- mk2/simd/ipp/function/1d/statistical.hpp
+++ mk2/simd/ipp/function/1d/statistical.hpp
@@ -28,14 +28,28 @@ namespace mk2 { namespace simd { namespace ipp { namespace function {
     #undef IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_SIGNATURE
     #undef IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_ARGS
     
+    #undef IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_ASSOCIATOR
+    
+    
+    #define IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_ASSOCIATOR(base_struct, base_func, descriptor)                            \
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func##_32f##descriptor, Ipp32f, Ipp32f, Ipp32f)            \
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func##_32fc##descriptor, Ipp32fc, Ipp32fc, Ipp32fc)        \
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func##_32f32fc##descriptor, Ipp32f, Ipp32fc, Ipp32fc)      \
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func##_32f64f##descriptor, Ipp32f, Ipp32f, Ipp64f)         \
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func##_32fc64fc##descriptor, Ipp32fc, Ipp32fc, Ipp64fc)    \
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func##_32f32fc64fc##descriptor, Ipp32f, Ipp32fc, Ipp64fc)  \
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func##_64f##descriptor, Ipp64f, Ipp64f, Ipp64f)            \
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func##_64fc##descriptor, Ipp64fc, Ipp64fc, Ipp64fc)        \
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func##_64f64fc##descriptor, Ipp64f, Ipp64fc, Ipp64fc)
+    
     // not inplace
     #define IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_SIGNATURE \
-    const Type* psrc1, const Type* psrc2, int len, Type* pdp
+    const Type1* psrc1, const Type2* psrc2, int len, Type3* pdp
     
     #define IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_ARGS   \
     psrc1, psrc2, len, pdp
     
-    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE(ipps_dot_prod, ippsDotProd, , Type)
+    IPP_FUNCTIONS_REPLACE_TO_TEMPLATE(ipps_dot_prod, ippsDotProd, , Type1, Type2, Type3)
     
     #undef IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_SIGNATURE
     #undef IPP_FUNCTIONS_REPLACE_TO_TEMPLATE_ARGS
