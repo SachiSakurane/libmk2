773277da00e5018f7864e4fe0622ef84598c9891
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-09-14 07:14:05 +0000

    ring_buffer.hpp edited online with Bitbucket

--- mk2/simd/ipp/ring_buffer.hpp
+++ mk2/simd/ipp/ring_buffer.hpp
@@ -6,10 +6,22 @@
 
 #include <mk2/container/fixed_array.hpp>
 #include <mk2/container/container_traits.hpp>
+#include <mk2/simd/ipp/function/arithmetric.hpp>
 #include <mk2/simd/ipp/function/initialization.hpp>
 
 namespace mk2 { namespace simd { namespace ipp
 {
+    // OperationFunc: f(Type bufptr, size index, int len, args...)
+    namespace ring_buffer_operation
+    {
+        decltype(auto) add()
+        {
+            return [](auto* bufptr, size_t index, int len, const auto* src)
+            { function::ipps_add_inplace(src + index, bufptr, len); };
+        }
+    }
+
+
     template<class Type>
     class ring_buffer
     {
@@ -19,47 +31,42 @@ namespace mk2 { namespace simd { namespace ipp
     public:
         ring_buffer(size_type size, Type v) : size_(size), index_(0), container_(size, v) {}
         
-        void set_sample(const Type* sample, size_type size)
+        void set_sample(const Type* sample, size_t len)
         {
             auto data = container_.data();
-            if (size + index_ < size_)
+            if (len + index_ < size_)
             {
-                function::ipps_copy(sample, data + index_, static_cast<int>(size));
-                index_ += size;
+                function::ipps_copy(sample, data + index_, static_cast<int>(len));
+                index_ += len;
             }
-            else if(size + index_ == size_)
+            else if(len + index_ == size_)
             {
-                function::ipps_copy(sample, data + index_, static_cast<int>(size));
+                function::ipps_copy(sample, data + index_, static_cast<int>(len));
                 index_ = 0;
             }
             else
             {
                 auto section_size = size_ - index_;
                 function::ipps_copy(sample, data + index_, static_cast<int>(section_size));
-                index_ = size - section_size;
+                index_ = len - section_size;
                 function::ipps_copy(sample + section_size, data, static_cast<int>(index_));
             }
         }
-    
-        void set_move_sample(const Type* sample, size_type size)
+        
+        // OperationFunc: f(Type bufptr, size index, int len, args...)
+        template<class OperationFunc, class Args...>
+        void operation(OperationFunc&& f, size_t len, Args&&... args)
         {
             auto data = container_.data();
-            if (size + index_ < size_)
+            if (len + index_ < size_)
             {
-                function::ipps_move(sample, data + index_, static_cast<int>(size));
-                index_ += size;
-            }
-            else if(size + index_ == size_)
-            {
-                function::ipps_move(sample, data + index_, static_cast<int>(size));
-                index_ = 0;
+                std::forward<OperationFunc>(f)(data + index_, 0, static_cast<int>(len), std::forward<Args>(args)...);
             }
             else
             {
                 auto section_size = size_ - index_;
-                function::ipps_move(sample, data + index_, static_cast<int>(section_size));
-                index_ = size - section_size;
-                function::ipps_move(sample + section_size, data, static_cast<int>(index_));
+                std::forward<OperationFunc>(f)(data + index_, 0, static_cast<int>(section_size), std::forward<Args>(args)...);
+                std::forward<OperationFunc>(f)(data, section_size, static_cast<int>(len - section_size), std::forward<Args>(args)...);
             }
         }
         
