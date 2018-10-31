719052347793c61e8953c87729cf2c8b444cd695
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-10-31 06:28:03 +0000

    alignment.hpp created online with Bitbucket

---
+++ mk2/simd/intrin/alignment.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Himatya on 2018-10-31.
+//
+
+#pragma once
+
+#include <traits>
+
+namespace mk2 { namespace simd { namespace intrin {
+    template<Bool B> struct is_align<B>
+    {
+        static constexpr bool value = B;
+    }
+    
+}}}
