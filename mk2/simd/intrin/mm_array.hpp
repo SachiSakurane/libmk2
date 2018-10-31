49c776ae1bbe12010678b75b5e3b8a7aa07377cf
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-10-31 08:34:10 +0000

    mm_array.hpp edited online with Bitbucket

--- mk2/simd/intrin/mm_array.hpp
+++ mk2/simd/intrin/mm_array.hpp
@@ -6,14 +6,14 @@
 
 #include <mk2/simd/intrin/include.hpp>
 #include <mk2/simd/intrin/mm_type.hpp>
-#include <mk2/simd/intrin/function/load.hpp>
-#include <mk2/simd/intrin/function/setzero.hpp>
-#include <mk2/simd/intrin/function/store.hpp>
+#include <mk2/simd/intrin/wrapper/assignment.hpp>
+#include <mk2/simd/intrin/wrapper/setzero.hpp>
 
 namespace mk2 { namespace simd { namespace intrin
 {
-
-    template <class Type, class Bits, size_t Size, bool Aligned = false>
+    
+    // 外見の振る舞いとしてはType型のコンテナのようにふるまう
+    template <class Type, class Bits, size_t Size, class Align = mk2::simd::intrin::is_aligned<false>>
     class mm_array
     {
         using mm_type = mk2::simd::intrin::mm_type<Type, Bits>::type;
@@ -52,56 +52,118 @@ namespace mk2 { namespace simd { namespace intrin
         mm_array& operator=(mm_array&& )noexcept = default;
         
         pointer operator=(pointer lhv);
+        
+        // iterator
+        iterator begin() noexcept { return iterator(*this, 0); }
 
+        const_iterator begin() const noexcept { return const_iterator(*this, 0); }
 
-        // 
-        void store(pointer obj);
+        iterator end() noexcept { return iterator(*this, size_); }
+
+        const_iterator end() const noexcept { return const_iterator(*this, size_); }
+
+        reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+
+        const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
+
+        reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+
+        const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
+
+        const_iterator cbegin() const noexcept { return begin(); }
+
+        const_iterator cend() const noexcept { return end(); }
+
+        const_reverse_iterator crbegin() const noexcept { return rbegin(); }
+
+        const_reverse_iterator crend() const noexcept { return rend(); }
+
+        // access
+        reference operator[](size_t n) noexcept { return elems_[n / mm_elems_size][n % mm_contain_size]; }
+
+        const_reference operator[](size_t n) const noexcept { return elems_[n / mm_elems_size][n % mm_contain_size]; }
+
+        reference at(size_t n)
+        { return n < size_ ? (*this)[n] : throw std::out_of_range("fixed_array::at");}
+
+        const_reference at(size_t n) const
+        { return n < size_ ? (*this)[n] : throw std::out_of_range("fixed_array::at"); }
+        
+        reference front() noexcept { return (*this)[0]; }
+        const_reference front() const noexcept { return (*this)[0]; }
+
+        reference back() noexcept { return (*this)[Size > 0 ? Size - 1 : 0]; }
+        const_reference back() const noexcept { return (*this)[Size > 0 ? Size - 1 : 0]; }
+        
+        pointer data() { return reinterpret_cast<value_type*>(elems_); }
+        const_pointer data() const { return reinterpret_cast<value_type*>(elems_); }
+        
+        mm_type* mm_data() noexcept { return elems_; }
+        const mm_type* mm_data() const noexcept { return elems_; }
+        
+        // capacity
+        constexpr size_type size() const noexcept { return Size; }
+        constexpr size_type max_size() const noexcept { return Size; }
+        constexpr bool empty() const noexcept { return Size == 0; }
+        
+        // operation
+        template <class Container>
+        void load(const Container& p);
+        void load(pointer p);
+        
+        template <class Container>
+        void store(const Container& c);
+        void store(pointer p);
+        void fill(Type value);
+        void swap() noexcept(std::is_nothrow_swappable<mm_type>::value);
         
     private:
         template<size_t... Indices>
         mm_array(const Type* data, std::index_sequence<Indices...>);
     
         mm_type elems_[mm_elems_size];
-
     };
     
     // constructors
-    template <class Type, class Bits, size_t Size, bool Aligned>
-    mm_array<Type, Bits, Size, Aligned>::mm_array() : elems_{ {mk2::simd::intrin::function::setzero<mm_type>()} }
+    template <class Type, class Bits, size_t Size, class Align>
+    mm_array<Type, Bits, Size, Align>::mm_array() : elems_{ {mk2::simd::intrin::function::setzero<mm_type>()} }
     {}
     
-    template <class Type, class Bits, size_t Size, bool Aligned>
+    template <class Type, class Bits, size_t Size, class Align>
     template <class Container>
-    mm_array<Type, Bits, Size, Aligned>::mm_array(const Container& c) : mm_array{ c.data() }
+    mm_array<Type, Bits, Size, Align>::mm_array(const Container& c) : mm_array{ c.data() }
     {
         static_assert(c.size() >= Size);
     }
     
-    template <class Type, class Bits, size_t Size, bool Aligned>
-    mm_array<Type, Bits, Size, Aligned>::mm_array(const Type* data) : mm_array(data, std::make_index_sequence<Size>())
+    template <class Type, class Bits, size_t Size, class Align>
+    mm_array<Type, Bits, Size, Align>::mm_array(const Type* data) : mm_array(data, std::make_index_sequence<Size>())
     {}
     
-    template <class Type, class Bits, size_t Size, bool Aligned>
+    template <class Type, class Bits, size_t Size, class Align>
     template<size_t... Indices>
-    mm_array<Type, Bits, Size, Aligned>::mm_array(const Type* data, std::index_sequence<Indices...>)
-        : elems_{mk2::simd::intrin::function::load<Bits, Aligned>(data + Indices * mm_contain_size)...}
+    mm_array<Type, Bits, Size, Align>::mm_array(const Type* data, std::index_sequence<Indices...>)
+        : elems_{mk2::simd::intrin::function::load<Bits, Align>(data + Indices * mm_contain_size)...}
     {}
     
     // assignment operator
-    template <class Type, class Bits, size_t Size, bool Aligned>
-    typename mm_array<Type, Bits, Size, Aligned>::pointer mm_array<Type, Bits, Size>::operator=(typename mm_array<Type, Bits, Size, Aligned>::pointer lhv)
+    template <class Type, class Bits, size_t Size, class Align>
+    typename mm_array<Type, Bits, Size, Align>::pointer mm_array<Type, Bits, Size, Align>::operator=(typename mm_array<Type, Bits, Size, Align>::pointer lhv)
     {
         this->store(lhv);
         return lhv;
     }
     
-    // 
-    template <class Type, class Bits, size_t Size, bool Aligned>
-    void mm_array<Type, Bits, Size>::store(typename mm_array<Type, Bits, Size, Aligned>::pointer obj)
+    // operation
+    template <class Type, class Bits, size_t Size, class Align>
+    void mm_array<Type, Bits, Size, Align>::store(typename mm_array<Type, Bits, Size, Align>::pointer obj)
     {
         for (std::size_t i = 0; i < mm_elems_size; i++)
         {
-            mk2::simd::intrin::function::store<Aligned>(lhv + i * mm_contain_size, elems_ + i);
+            mk2::simd::intrin::function::store<Align>(lhv + i * mm_contain_size, elems_ + i);
         }
     }
+    
+    // operator
+    
 }}}
