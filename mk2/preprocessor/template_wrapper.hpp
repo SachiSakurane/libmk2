2e4ce82fa9c8052df69df249ef0b1a5b6fd1c468
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-11-07 08:10:47 +0000

    template_wrapper.hpp edited online with Bitbucket

--- mk2/preprocessor/template_wrapper.hpp
+++ mk2/preprocessor/template_wrapper.hpp
@@ -14,34 +14,34 @@
 #define MK2_PP_ADD_CLASS_IMPL(first, last, ...) BOOST_PP_FOR((first, last, (__VA_ARGS__)), MK2_PP_ADD_CLASS_IMPL_FOR_PRED, MK2_PP_ADD_CLASS_IMPL_FOR_OP, MK2_PP_ADD_CLASS_IMPL_FOR_MACRO)
 #define MK2_PP_ADD_CLASS(...) MK2_PP_ADD_CLASS_IMPL(0, BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), __VA_ARGS__)
 
-#define MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func, template_value)                 \
-template<> struct base_struct<MK2_PP_UNPACK_TUPLE(template_value)>                              \
-{                                                                                               \
-    template<class ...Args> static decltype(auto) func(Args&&... args)                          \
-    { return base_func(std::forward<Args>(args)...); }                                          \
+#define MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, base_func, template_value) \
+template<> struct base_struct<BOOST_PP_TUPLE_ENUM(template_value)>              \
+{                                                                               \
+    template<class ...Args> static decltype(auto) func(Args&&... args)          \
+    { return base_func(std::forward<Args>(args)...); }                          \
 };
 
-#define MK2_PP_REPLACE_TO_TEMPLATE(func_name, base_func, signature, args, template_name)                    \
-MK2_PP_REPLACE_TO_TEMPLATE_REDUNDANCY(func_name, base_func, signature, args, template_name, template_name)
+#define MK2_PP_REPLACE_TO_TEMPLATE(func_name, base_func, signature, args, template_name, associator)                    \
+MK2_PP_REPLACE_TO_TEMPLATE_REDUNDANCY(func_name, base_func, signature, args, template_name, template_name, associator)
 
-#define MK2_PP_REPLACE_TO_TEMPLATE_REDUNDANCY(func_name, base_func, signature, args, template_name, template_init)  \
-namespace detail                                                                                            \
-{                                                                                                           \
-    template<MK2_PP_ADD_CLASS(MK2_PP_UNPACK_TUPLE(template_name))> struct func_name##_impl{};               \
-    MK2_PP_REPLACE_TO_TEMPLATE_ASSOCIATOR(func_name##_impl, base_func)                                      \
-}                                                                                                           \
-template<MK2_PP_ADD_CLASS(MK2_PP_UNPACK_TUPLE(template_init))>                                              \
-inline decltype(auto) func_name(MK2_PP_UNPACK_TUPLE(signature))                                             \
-{ return detail::func_name##_impl<MK2_PP_UNPACK_TUPLE(template_name)>::func(MK2_PP_UNPACK_TUPLE(args)); }
+#define MK2_PP_REPLACE_TO_TEMPLATE_REDUNDANCY(func_name, base_func, signature, args, template_name, template_init, associator)  \
+namespace detail                                                                                \
+{                                                                                               \
+    template<MK2_PP_ADD_CLASS(BOOST_PP_TUPLE_ENUM(template_name))> struct func_name##_impl{};   \
+    associator(func_name##_impl, base_func)                                                     \
+}                                                                                               \
+template<MK2_PP_ADD_CLASS(BOOST_PP_TUPLE_ENUM(template_init))>                                  \
+inline decltype(auto) func_name(BOOST_PP_TUPLE_ENUM(signature))                                 \
+{ return detail::func_name##_impl<BOOST_PP_TUPLE_ENUM(template_name)>::func(BOOST_PP_TUPLE_ENUM(args)); }
 
-#define MK2_PP_REPLACE_TO_TEMPLATE_ADDITIVE(func_name, base_func, signature, args, template_name)   \
-MK2_PP_REPLACE_TO_TEMPLATE_ADDITIVE_REDUNDANCY(func_name, base_func, signature, args, template_name, template_name)
+#define MK2_PP_REPLACE_TO_TEMPLATE_ADDITIVE(func_name, base_func, signature, args, template_name, associator)   \
+MK2_PP_REPLACE_TO_TEMPLATE_ADDITIVE_REDUNDANCY(func_name, base_func, signature, args, template_name, template_name, associator)
 
-#define MK2_PP_REPLACE_TO_TEMPLATE_ADDITIVE_REDUNDANCY(func_name, base_func, signature, args, template_name, template_init) \
-namespace detail                                                                                                \
-{                                                                                                               \
-    MK2_PP_REPLACE_TO_TEMPLATE_ASSOCIATOR(func_name##_impl, base_func)                                          \
-}                                                                                                               \
-template<MK2_PP_ADD_CLASS(MK2_PP_UNPACK_TUPLE(template_init))>                                                  \
-inline decltype(auto) func_name(MK2_PP_UNPACK_TUPLE(signature))                                                 \
-{ return detail::func_name##_impl<MK2_PP_UNPACK_TUPLE(template_name)>::func(MK2_PP_UNPACK_TUPLE(args)); }
+#define MK2_PP_REPLACE_TO_TEMPLATE_ADDITIVE_REDUNDANCY(func_name, base_func, signature, args, template_name, template_init, associator) \
+namespace detail                                                \
+{                                                               \
+    associator(func_name##_impl, base_func)                     \
+}                                                               \
+template<MK2_PP_ADD_CLASS(BOOST_PP_TUPLE_ENUM(template_init))>  \
+inline decltype(auto) func_name(BOOST_PP_TUPLE_ENUM(signature)) \
+{ return detail::func_name##_impl<BOOST_PP_TUPLE_ENUM(template_name)>::func(BOOST_PP_TUPLE_ENUM(args)); }
