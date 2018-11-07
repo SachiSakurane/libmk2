8c2facd03290829d5c8ac7237ac4e60c35834d20
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-11-07 07:45:53 +0000

    arithmetic.hpp edited online with Bitbucket

--- mk2/simd/intrin/wrapper/intel/arithmetic.hpp
+++ mk2/simd/intrin/wrapper/intel/arithmetic.hpp
@@ -6,16 +6,17 @@
 
 #include <mk2/preprocessor/template_wrapper.hpp>
 #include <mk2/simd/intrin/include.hpp>
+#include <mk2/simd/intrin/utility/archtecture_requirement.hpp>
 
 namespace mk2 { namespace simd { namespace intrin { namespace wrapper { namespace intel {
-
-    #define MK2_PP_REPLACE_TO_TEMPLATE_ASSOCIATOR(base_struct, base_func)               \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm_##base_func##_ps,       (__m128))  \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm_##base_func##_pd,       (__m128d)) \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm256_##base_func##_ps,    (__m256))  \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm256_##base_func##_pd,    (__m256d)) \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm512_##base_func##_ps,    (__m512))  \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm512_##base_func##_pd,    (__m512d))
+	
+    #define MK2_PP_REPLACE_TO_TEMPLATE_ASSOCIATOR(base_struct, base_func)                                                   \
+    MK2_ARCHTECTURE_REQUIREMENT_SSE2(   MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm_##base_func##_ps,       (__m128))) \
+    MK2_ARCHTECTURE_REQUIREMENT_SSE2(   MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm_##base_func##_pd,       (__m128d)))\
+    MK2_ARCHTECTURE_REQUIREMENT_AVX(    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm256_##base_func##_ps,    (__m256))) \
+	MK2_ARCHTECTURE_REQUIREMENT_AVX(    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm256_##base_func##_pd,    (__m256d)))\
+    MK2_ARCHTECTURE_REQUIREMENT_AVX512F(MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm512_##base_func##_ps,    (__m512))) \
+    MK2_ARCHTECTURE_REQUIREMENT_AVX512F(MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm512_##base_func##_pd,    (__m512d)))
 
     MK2_PP_REPLACE_TO_TEMPLATE(add, add, (RegisterType a, RegisterType b), (a, b), (RegisterType))
     MK2_PP_REPLACE_TO_TEMPLATE(sub, sub, (RegisterType a, RegisterType b), (a, b), (RegisterType))
@@ -24,11 +25,11 @@ namespace mk2 { namespace simd { namespace intrin { namespace wrapper { namespac
 
     #undef MK2_PP_REPLACE_TO_TEMPLATE_ASSOCIATOR
 
-    #define MK2_PP_REPLACE_TO_TEMPLATE_ASSOCIATOR(base_struct, base_func)               \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm_##base_func##_ss,       (__m128))  \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm_##base_func##_sd,       (__m128d)) \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm256_##base_func##_ss,    (__m256))  \
-    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm256_##base_func##_sd,    (__m256d))
+    #define MK2_PP_REPLACE_TO_TEMPLATE_ASSOCIATOR(base_struct, base_func)                                                   \
+    MK2_ARCHTECTURE_REQUIREMENT_SSE2(   MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm_##base_func##_ss,       (__m128))) \
+    MK2_ARCHTECTURE_REQUIREMENT_SSE2(   MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm_##base_func##_sd,       (__m128d)))\
+    MK2_ARCHTECTURE_REQUIREMENT_AVX(    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm256_##base_func##_ss,    (__m256))) \
+    MK2_ARCHTECTURE_REQUIREMENT_AVX(    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm256_##base_func##_sd,    (__m256d)))
 
     MK2_PP_REPLACE_TO_TEMPLATE(add_s, add, (RegisterType a, RegisterType b), (a, b), (RegisterType))
     MK2_PP_REPLACE_TO_TEMPLATE(sub_s, sub, (RegisterType a, RegisterType b), (a, b), (RegisterType))
@@ -36,5 +37,5 @@ namespace mk2 { namespace simd { namespace intrin { namespace wrapper { namespac
     MK2_PP_REPLACE_TO_TEMPLATE(mul_s, mul, (RegisterType a, RegisterType b), (a, b), (RegisterType))
 
     #undef MK2_PP_REPLACE_TO_TEMPLATE_ASSOCIATOR
-
+	
 }}}}}
