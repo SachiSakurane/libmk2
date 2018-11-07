16b722c299ee84c042500fd8bcba01a5ed8e0df0
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-11-07 08:42:27 +0000

    logical.hpp edited online with Bitbucket

--- mk2/simd/intrin/wrapper/intel/logical.hpp
+++ mk2/simd/intrin/wrapper/intel/logical.hpp
@@ -19,7 +19,7 @@ namespace mk2 { namespace simd { namespace intrin { namespace wrapper { namespac
     MK2_ARCHTECTURE_REQUIREMENT_AVX2(    MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm256_##base_func##_si256, (__m256i)))   \
     MK2_ARCHTECTURE_REQUIREMENT_AVX512DQ(MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm512_##base_func##_ps,    (__m512)))    \
     MK2_ARCHTECTURE_REQUIREMENT_AVX512DQ(MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm512_##base_func##_pd,    (__m512d)))   \
-    MK2_ARCHTECTURE_REQUIREMENT_AVX512DQ(MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm512_##base_func##_si512, (__m512i)))
+    MK2_ARCHTECTURE_REQUIREMENT_AVX512F( MK2_PP_REPLACE_TO_TEMPLATE_FUNC(base_struct, _mm512_##base_func##_si512, (__m512i)))
 
     MK2_PP_REPLACE_TO_TEMPLATE(bit_and,    and,    (Type a, Type b), (a, b), (Type), MK2_LOGICAL_ASSOCIATOR)
     MK2_PP_REPLACE_TO_TEMPLATE(bit_andnot, andnot, (Type a, Type b), (a, b), (Type), MK2_LOGICAL_ASSOCIATOR)
