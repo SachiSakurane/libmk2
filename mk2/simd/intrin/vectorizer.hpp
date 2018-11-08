6ac8410ac1c6236ba8d6ca7c8bbdab09fcbabce2
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-11-08 08:14:40 +0000

    vectorizer.hpp edited online with Bitbucket

--- mk2/simd/intrin/vectorizer.hpp
+++ mk2/simd/intrin/vectorizer.hpp
@@ -4,6 +4,9 @@
 
 #pragma once
 
+#include <cstddef>
+#include <iterator>
+
 #include <mk2/iterator/data.hpp>
 #include <mk2/iterator/index_iterator.hpp>
 #include <mk2/simd/intrin/detail/vectorizer_buffer.hpp>
@@ -13,53 +16,52 @@
 #include <mk2/simd/intrin/function/logical.hpp>
 #include <mk2/simd/intrin/function/set.hpp>
 #include <mk2/simd/intrin/function/swap.hpp>
+#include <mk2/simd/intrin/type_traits/alignment.hpp>
 #include <mk2/simd/intrin/type_traits/bit_type.hpp>
 
 namespace mk2 { namespace simd { namespace intrin
 {
-    // 外見の振る舞いとしてはType型のコンテナのようにふるまう
-    template <class Type, class Bits, size_t Size, class Align = mk2::simd::intrin::is_aligned<false>>
+    template <class Bits, class Type, std::size_t Size>
     class vectorizer
     {
     public:
-        using mm_type = typename mk2::simd::intrin::bit_type<Type, Bits>::type;
-        static constexpr std::size_t mm_contain_size = sizeof(mm_type) / sizeof(Type);
-        static constexpr std::size_t mm_elems_size = Size / mm_contain_size;
-
         using value_type = Type;
-        using reference = value_type&;
+        using reference = value_type & ;
         using const_reference = const value_type&;
         using iterator = mk2::iterator::index_iterator<vectorizer>;
         using const_iterator = mk2::iterator::index_iterator<const vectorizer>;
-        using pointer = value_type*;
+        using pointer = value_type * ;
         using const_pointer = const value_type*;
         using size_type = std::size_t;
         using difference_type = std::ptrdiff_t;
         using reverse_iterator = std::reverse_iterator<iterator>;
         using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-        //using allocator_type = Allocator;
-    
+
+        using mm_type = typename mk2::simd::intrin::bit_type<value_type, Bits>::type;
+        static constexpr std::size_t mm_contain_size = sizeof(mm_type) / sizeof(value_type);
+        static constexpr std::size_t mm_elems_size = Size / mm_contain_size;
+
         // constructors
         vectorizer();
-        
-        vectorizer(const vectorizer& ) = default;
-        vectorizer(vectorizer&& ) noexcept = default;
+
+        vectorizer(const vectorizer&) = default;
+        vectorizer(vectorizer&&) noexcept = default;
 
         template <class Container>
         explicit vectorizer(const Container& c);
         explicit vectorizer(const_pointer data);
         explicit vectorizer(mm_type* mm_data);
-        explicit vectorizer(detail::vectorizer_buffer<Type, Bits, Size, Align>&& buf);
+        explicit vectorizer(detail::vectorizer_buffer<Bits, Type, Size>&& buf);
 
         // destructor
         virtual ~vectorizer() = default;
-        
-        // assignment operator
-        vectorizer& operator=(const vectorizer& ) = default;
-        vectorizer& operator=(vectorizer&& )noexcept = default;
 
-        vectorizer& operator=(detail::vectorizer_buffer<Type, Bits, Size, Align>&& buf);
-        
+        // assignment operator
+        vectorizer& operator=(const vectorizer&) = default;
+        vectorizer& operator=(vectorizer&&)noexcept = default;
+
+        vectorizer& operator=(detail::vectorizer_buffer<Bits, Type, Size>&& buf);
+
         // iterator
         iterator begin() noexcept { return iterator(*this, 0); }
 
@@ -86,111 +88,116 @@ namespace mk2 { namespace simd { namespace intrin
         const_reverse_iterator crend() const noexcept { return rend(); }
 
         // access
-        reference operator[](size_t n) noexcept { return data()[n]; }
+        reference operator[](std::size_t n) noexcept { return data()[n]; }
 
-        const_reference operator[](size_t n) const noexcept { return data()[n]; }
+        const_reference operator[](std::size_t n) const noexcept { return data()[n]; }
 
-        reference at(size_t n)
-        { return n < Size ? (*this)[n] : throw std::out_of_range("fixed_array::at"); }
+        reference at(std::size_t n)
+        {
+            return n < Size ? (*this)[n] : throw std::out_of_range("fixed_array::at");
+        }
 
-        const_reference at(size_t n) const
-        { return n < Size ? (*this)[n] : throw std::out_of_range("fixed_array::at"); }
-        
+        const_reference at(std::size_t n) const
+        {
+            return n < Size ? (*this)[n] : throw std::out_of_range("fixed_array::at");
+        }
+
         reference front() noexcept { return (*this)[0]; }
         const_reference front() const noexcept { return (*this)[0]; }
 
         reference back() noexcept { return (*this)[Size > 0 ? Size - 1 : 0]; }
         const_reference back() const noexcept { return (*this)[Size > 0 ? Size - 1 : 0]; }
-        
+
         pointer data() { return reinterpret_cast<value_type*>(elems_); }
         const_pointer data() const { return reinterpret_cast<value_type*>(elems_); }
-        
+
         mm_type* mm_data() noexcept { return elems_; }
         const mm_type* mm_data() const noexcept { return elems_; }
-        
+
         // capacity
         constexpr size_type size() const noexcept { return Size; }
         constexpr size_type max_size() const noexcept { return Size; }
         constexpr bool empty() const noexcept { return Size == 0; }
-        
+
         // operation
-        template <class Container>
-        void load(const Container& p);
-        void load(pointer p);
-        
-        template <class Container>
-        void store(const Container& c);
-        void store(pointer p);
+        template <class Container, class Align> void load(const Container& p);
+        template <class Align>                  void load(pointer p);
+
+        template <class Container, class Align> void store(const Container& c);
+        template <class Align>                  void store(pointer p);
+
         void fill(value_type value);
-        void swap(vectorizer& other) noexcept(std::is_nothrow_swappable<mm_type>::value);
-        
+        void swap(vectorizer& other) noexcept(std::is_nothrow_swappable<vectorizer<Bits, Type, Size>::mm_type>::value);
+
     private:
-        template<size_t... Indices>
+
+        template<std::size_t... Indices>
         vectorizer(const Type* data, std::index_sequence<Indices...>);
 
-        template<size_t... Indices>
+        template<std::size_t... Indices>
         vectorizer(mm_type* data, std::index_sequence<Indices...>);
 
         mm_type elems_[mm_elems_size];
     };
-    
+
     // constructors
-    template <class Type, class Bits, size_t Size, class Align>
-    vectorizer<Type, Bits, Size, Align>::vectorizer() : elems_{ {mk2::simd::intrin::function::setzero<Bits, Type>()} }
+    template <class Bits, class Type, std::size_t Size>
+    vectorizer<Bits, Type, Size>::vectorizer() : elems_{ { mk2::simd::intrin::function::setzero<Bits, Type>() } }
     {}
-    
-    template <class Type, class Bits, size_t Size, class Align>
+
+    template <class Bits, class Type, std::size_t Size>
     template <class Container>
-    vectorizer<Type, Bits, Size, Align>::vectorizer(const Container& c) : vectorizer(c.data())
+    vectorizer<Bits, Type, Size>::vectorizer(const Container& c) : vectorizer(c.data())
     {
         // static_assert(c.size() >= Size);
     }
+
+    template <class Bits, class Type, std::size_t Size>
+    vectorizer<Bits, Type, Size>::vectorizer(const Type* data)
+        : vectorizer(data, std::make_index_sequence<vectorizer<Bits, Type, Size>::mm_elems_size>())
+    {}
+
+    template <class Bits, class Type, std::size_t Size>
+    template<std::size_t... Indices>
+    vectorizer<Bits, Type, Size>::vectorizer(const Type* data, std::index_sequence<Indices...>)
+        : elems_{ mk2::simd::intrin::function::load<Bits, Type>(data + Indices * mm_contain_size)... }
+    {}
+
+    template <class Bits, class Type, std::size_t Size>
+    vectorizer<Bits, Type, Size>::vectorizer(typename vectorizer<Bits, Type, Size>::mm_type* data)
+        : vectorizer(data, std::make_index_sequence<vectorizer<Bits, Type, Size>::mm_elems_size>())
+    {}
+
+    template <class Bits, class Type, std::size_t Size>
+    template<std::size_t... Indices>
+    vectorizer<Bits, Type, Size>::vectorizer(typename vectorizer<Bits, Type, Size>::mm_type* data, std::index_sequence<Indices...>)
+        : elems_{ data[Indices]... }
+    {}
     
-    template <class Type, class Bits, size_t Size, class Align>
-    vectorizer<Type, Bits, Size, Align>::vectorizer(const Type* data)
-            : vectorizer(data, std::make_index_sequence<vectorizer<Type, Bits, Size, Align>::mm_elems_size>())
+    template <class Bits, class Type, std::size_t Size>
+    vectorizer<Bits, Type, Size>::vectorizer(detail::vectorizer_buffer<Bits, Type, Size>&& buf)
+        : vectorizer(buf.container, std::make_index_sequence<vectorizer<Bits, Type, Size>::mm_elems_size>())
     {}
     
-    template <class Type, class Bits, size_t Size, class Align>
-    template<size_t... Indices>
-    vectorizer<Type, Bits, Size, Align>::vectorizer(const Type* data, std::index_sequence<Indices...>)
-            : elems_{ mk2::simd::intrin::function::load<Bits, Type, Align>(data + Indices * mm_contain_size)... }
-    {}
-
-    template <class Type, class Bits, size_t Size, class Align>
-    vectorizer<Type, Bits, Size, Align>::vectorizer(typename vectorizer<Type, Bits, Size, Align>::mm_type* data)
-            : vectorizer(data, std::make_index_sequence<vectorizer<Type, Bits, Size, Align>::mm_elems_size>())
-    {}
-
-    template <class Type, class Bits, size_t Size, class Align>
-    template<size_t... Indices>
-    vectorizer<Type, Bits, Size, Align>::vectorizer(typename vectorizer<Type, Bits, Size, Align>::mm_type* data, std::index_sequence<Indices...>)
-            : elems_{ data[Indices]... }
-    {}
-
-    template <class Type, class Bits, size_t Size, class Align>
-    vectorizer<Type, Bits, Size, Align>::vectorizer(detail::vectorizer_buffer<Type, Bits, Size, Align>&& buf)
-            : vectorizer(buf.container, std::make_index_sequence<vectorizer<Type, Bits, Size, Align>::mm_elems_size>())
-    {}
-
     // assign
-    template <class Type, class Bits, size_t Size, class Align>
-    vectorizer<Type, Bits, Size, Align>& vectorizer<Type, Bits, Size, Align>::operator=(detail::vectorizer_buffer<Type, Bits, Size, Align>&& buf)
+    template <class Bits, class Type, std::size_t Size>
+    vectorizer<Bits, Type, Size>& vectorizer<Bits, Type, Size>::operator=(detail::vectorizer_buffer<Bits, Type, Size>&& buf)
     {
-        *this = vectorizer<Type, Bits, Size, Align>(std::move(buf));
+        *this = vectorizer<Bits, Type, Size>(std::move(buf));
         return *this;
     }
     
     // operation
-    template <class Type, class Bits, size_t Size, class Align>
-    template <class Container>
-    void vectorizer<Type, Bits, Size, Align>::load(const Container& c)
+    template <class Bits, class Type, std::size_t Size>
+    template <class Container, class Align>
+    void vectorizer<Bits, Type, Size>::load(const Container& c)
     {
-        this->load(mk2::iterator::data(c));
+        this->load<Align>(mk2::iterator::data(c));
     }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    void vectorizer<Type, Bits, Size, Align>::load(typename vectorizer<Type, Bits, Size, Align>::pointer p)
+    template <class Bits, class Type, std::size_t Size>
+    template <class Align>
+    void vectorizer<Bits, Type, Size>::load(typename vectorizer<Bits, Type, Size>::pointer p)
     {
         for (std::size_t i = 0; i < mm_elems_size; i++)
         {
@@ -198,15 +205,16 @@ namespace mk2 { namespace simd { namespace intrin
         }
     }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    template <class Container>
-    void vectorizer<Type, Bits, Size, Align>::store(const Container& c)
+    template <class Bits, class Type, std::size_t Size>
+    template <class Container, class Align>
+    void vectorizer<Bits, Type, Size>::store(const Container& c)
     {
-        this->store(mk2::iterator::data(c));
+        this->store<Align>(mk2::iterator::data(c));
     }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    void vectorizer<Type, Bits, Size, Align>::store(typename vectorizer<Type, Bits, Size, Align>::pointer p)
+    template <class Bits, class Type, std::size_t Size>
+    template <class Align>
+    void vectorizer<Bits, Type, Size>::store(typename vectorizer<Bits, Type, Size>::pointer p)
     {
         for (std::size_t i = 0; i < mm_elems_size; i++)
         {
@@ -214,8 +222,8 @@ namespace mk2 { namespace simd { namespace intrin
         }
     }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    void vectorizer<Type, Bits, Size, Align>::fill(Type value)
+    template <class Bits, class Type, std::size_t Size>
+    void vectorizer<Bits, Type, Size>::fill(Type value)
     {
         for (std::size_t i = 0; i < mm_elems_size; i++)
         {
@@ -223,77 +231,78 @@ namespace mk2 { namespace simd { namespace intrin
         }
     }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    void vectorizer<Type, Bits, Size, Align>::swap(vectorizer<Type, Bits, Size, Align>& other)
-        noexcept(std::is_nothrow_swappable<vectorizer<Type, Bits, Size, Align>::mm_type>::value)
+    template <class Bits, class Type, std::size_t Size>
+    void vectorizer<Bits, Type, Size>::swap(vectorizer<Bits, Type, Size>& other)
+        noexcept(std::is_nothrow_swappable<vectorizer<Bits, Type, Size>::mm_type>::value)
     {
         for (std::size_t i = 0; i < mm_elems_size; i++)
         {
             mk2::simd::intrin::function::swap(elems_[i], other.mm_data()[i]);
         }
     }
-    
+
     // comparison operator
-    template <class Type, class Bits, size_t Size, class Align>
-    inline bool operator==(const vectorizer<Type, Bits, Size, Align>& lhv, const vectorizer<Type, Bits, Size, Align>& rhv)
+    template <class Bits, class Type, std::size_t Size>
+    inline bool operator==(const vectorizer<Bits, Type, Size>& lhv, const vectorizer<Bits, Type, Size>& rhv)
     {
         decltype(auto) mlhv = lhv.mm_data();
         decltype(auto) mrhv = rhv.mm_data();
-        const auto zero = mk2::simd::intrin::function::setzero<vectorizer<Type, Bits, Size, Align>::mm_type>();
+        const auto zero = mk2::simd::intrin::function::setzero<Bits, Type>();
 
-        for (std::size_t i = 0; i < vectorizer<Type, Bits, Size, Align>::mm_elems_size; i++)
+        for (std::size_t i = 0; i < vectorizer<Bits, Type, Size>::mm_elems_size; i++)
         {
-            if (mk2::simd::intrin::function::testc(zero, mk2::simd::intrin::function::cmp_neq(mlhv[i], mrhv[i])))
+            if (mk2::simd::intrin::function::testc(zero, mk2::simd::intrin::function::cmp_eq(mlhv[i], mrhv[i])))
                 return false;
         }
         return true;
     }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    inline bool operator!=(const vectorizer<Type, Bits, Size, Align>& lhv, const vectorizer<Type, Bits, Size, Align>& rhv)
+    template <class Bits, class Type, std::size_t Size>
+    inline bool operator!=(const vectorizer<Bits, Type, Size>& lhv, const vectorizer<Bits, Type, Size>& rhv)
     {
         return !operator==(lhv, rhv);
     }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    inline bool operator<(const vectorizer<Type, Bits, Size, Align>& lhv, const vectorizer<Type, Bits, Size, Align>& rhv)
+    template <class Bits, class Type, std::size_t Size>
+    inline bool operator<(const vectorizer<Bits, Type, Size>& lhv, const vectorizer<Bits, Type, Size>& rhv)
     {
         decltype(auto) mlhv = lhv.mm_data();
         decltype(auto) mrhv = rhv.mm_data();
-        const auto zero = mk2::simd::intrin::function::setzero<vectorizer<Type, Bits, Size, Align>::mm_type>();
+        const auto zero = mk2::simd::intrin::function::setzero<vectorizer<Bits, Type, Size>::mm_type>();
 
-        for (std::size_t i = 0; i < vectorizer<Type, Bits, Size, Align>::mm_elems_size; i++)
+        for (std::size_t i = 0; i < vectorizer<Bits, Type, Size>::mm_elems_size; i++)
         {
-            if (mk2::simd::intrin::function::testc(zero, mk2::simd::intrin::function::cmp_nlt(mlhv[i], mrhv[i])))
+            if (mk2::simd::intrin::function::testc(zero, mk2::simd::intrin::function::cmp_lt(mlhv[i], mrhv[i])))
                 return false;
         }
         return true;
     }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    inline bool operator<=(const vectorizer<Type, Bits, Size, Align>& lhv, const vectorizer<Type, Bits, Size, Align>& rhv)
+    template <class Bits, class Type, std::size_t Size>
+    inline bool operator<=(const vectorizer<Bits, Type, Size>& lhv, const vectorizer<Bits, Type, Size>& rhv)
     {
         decltype(auto) mlhv = lhv.mm_data();
         decltype(auto) mrhv = rhv.mm_data();
-        const auto zero = mk2::simd::intrin::function::setzero<vectorizer<Type, Bits, Size, Align>::mm_type>();
+        const auto zero = mk2::simd::intrin::function::setzero<vectorizer<Bits, Type, Size>::mm_type>();
 
-        for (std::size_t i = 0; i < vectorizer<Type, Bits, Size, Align>::mm_elems_size; i++)
+        for (std::size_t i = 0; i < vectorizer<Bits, Type, Size>::mm_elems_size; i++)
         {
-            if (mk2::simd::intrin::function::testc(zero, mk2::simd::intrin::function::cmp_nle(mlhv[i], mrhv[i])))
+            if (mk2::simd::intrin::function::testc(zero, mk2::simd::intrin::function::cmp_le(mlhv[i], mrhv[i])))
                 return false;
         }
         return true;
     }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    inline bool operator>(const vectorizer<Type, Bits, Size, Align>& lhv, const vectorizer<Type, Bits, Size, Align>& rhv)
+    template <class Bits, class Type, std::size_t Size>
+    inline bool operator>(const vectorizer<Bits, Type, Size>& lhv, const vectorizer<Bits, Type, Size>& rhv)
     {
         return !operator<=(lhv, rhv);
-    } 
+    }
 
-    template <class Type, class Bits, size_t Size, class Align>
-    inline bool operator>=(const vectorizer<Type, Bits, Size, Align>& lhv, const vectorizer<Type, Bits, Size, Align>& rhv)
+    template <class Bits, class Type, std::size_t Size>
+    inline bool operator>=(const vectorizer<Bits, Type, Size>& lhv, const vectorizer<Bits, Type, Size>& rhv)
     {
         return !operator<(lhv, rhv);
     }
+
 }}}
