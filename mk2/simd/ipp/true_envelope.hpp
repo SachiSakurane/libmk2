2c764dc1b1680c086aaf32ee23cef666f2a011ee
Himatya <hakureinosuekiti@yahoo.co.jp>
2018-11-22 06:23:22 +0000

    true_envelope.hpp edited online with Bitbucket

--- mk2/simd/ipp/true_envelope.hpp
+++ mk2/simd/ipp/true_envelope.hpp
@@ -9,7 +9,6 @@
 #include <limits>
 
 #include <mk2/container/fixed_array.hpp>
-#include <mk2/simd/ipp/ipp_buffer_delete.hpp>
 #include <mk2/simd/ipp/type.hpp>
 #include <mk2/simd/ipp/fft.hpp>
 #include <mk2/simd/ipp/conversion.hpp>
@@ -22,53 +21,54 @@ namespace mk2 { namespace simd { namespace ipp {
     template <class Type>
     class true_envelope
     {
-        using ptr_type = std::unique_ptr<Type, ipp_buffer_delete>;
-        using spec_type = mk2::simd::ipp::ipps_fft_spec_c<Type>;
-    
     public:
         true_envelope(int order) : 
-            fft_(order), im_buffer_(1 << order), cepstrum_(1 << order)
+            fft_(order), calc_buffer_(1 << order), spectrum_(1 << order), cepstrum_(1 << order)
         {}
         
         ~true_envelope() = default;
         
-        IppStatus operator()(const Type* src_re, const Type* src_im, Type* dst, std::size_t iteration, std::size_t threshold, bool inverse = true)
+        IppStatus operator()(const Type* src_re, const Type* src_im, Type* dst, Type threshold, std::size_t lifter, std::size_t max_iteration = 16, bool inverse = true)
         {
             using ipf = mk2::simd::ipp::function;
-            const int size = static_cast<int>(im_buffer_.size());
+            const int size = static_cast<int>(calc_buffer_.size());
             auto err = ippStsNoErr;
             
-            // spectrum
-            fft_(src_re, src_im, dst, im_buffer_.data());
-            ipf::power_spectr(dst, im_buffer_.data(), dst, size);
-            ipf::ln(dst, dst, size, mk2::simd::ipp::precision_high);
+            // power spectrum
+            err |= fft_.forward(src_re, src_im, spectrum_.data(), calc_buffer_.data());
+            err |= ipf::power_spectr(spectrum_.data(), calc_buffer_.data(), spectrum_.data(), size);
+            //err |= ipf::log10(spectrum_.data(), spectrum_.data(), size, mk2::simd::ipp::precision_high);
             
-            if (iteration <= 1)
-                return err;
+            err |= ipf::copy(spectrum_.data(), dst, size);
             
-            ipf::copy(dst, cepstrum_.data(), size);
+            Type max_dif = std::numeric_limits<Type>::infnity();
             
             // true spectrum iteration
-            for (int i = 1; i < iteration; ++i)
+            for (std::size_t i = 1; i < max_iteration && max_dif > threshold; ++i)
             {
-                ipf::zero(im_buffer_.data(), size);
+                err |= ipf::zero(calc_buffer_.data(), size);
                 
-                if (inverse) fft_.inverse_inplace(cepstrum_.data(), im_buffer_.data());
-                else         fft_.inplace(cepstrum_.data(), im_buffer_.data());
+                if (inverse) err |= fft_.inverse(dst, calc_buffer_.data(), cepstrum_.data(), calc_buffer_.data());
+                else         err |= fft_.forward(dst, calc_buffer_.data(), cepstrum_.data(), calc_buffer_.data());
                 
-                ipf::zero(cepstrum_.data() + threshold, size - threshold);
+                // liftering
+                err |= ipf::zero(cepstrum_.data() + lifter, size - lifter);
                 
-                if (inverse) fft_.inplace(cepstrum_.data(), im_buffer_.data());
-                else         fft_.inverse_inplace(cepstrum_.data(), im_buffer_.data());
+                if (inverse) err |= fft_.forward_inplace(cepstrum_.data(), calc_buffer_.data());
+                else         err |= fft_.inverse_inplace(cepstrum_.data(), calc_buffer_.data());
                 
-                ipf::max_every_inplace(cepstrum_.data(), dst, size);
+                err |= ipf::max_every_inplace(cepstrum_.data(), dst, size);
+                
+                err |= ipf::sub(spectrum_.data(), dst, calc_buffer_.data());
+                err |= ipf::max(calc_buffer_.data(), size, &max_dif);
             }
             
             return err;
         }
         
     private:
-        mk2::container::fixed_array<Type> im_buffer_;
+        mk2::container::fixed_array<Type> calc_buffer_;
+        mk2::container::fixed_array<Type> spectrum_;
         mk2::container::fixed_array<Type> cepstrum_;
         mk2::simd::ipp::fft fft_;
         
